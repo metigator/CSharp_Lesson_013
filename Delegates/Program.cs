68c39412538a0ab0a6e017692664be49047b2783
inessSaa <inesssaada809@gmail.com>
2025-02-06 10:43:20 +0100

    013 Delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BeforeDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var emps = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Issam A", TotalSales = 65000m, Gender = "M"} ,
+                new Employee { Id = 2, Name = "Reem S", TotalSales = 50000m, Gender = "F"} ,
+                new Employee { Id = 3, Name = "Suzan B", TotalSales = 65000m, Gender = "F"} ,
+                new Employee { Id = 4, Name = "Sara A", TotalSales = 40000m, Gender = "F"} ,
+                new Employee { Id = 5, Name = "Salah S", TotalSales = 42000m, Gender = "M"} ,
+                new Employee { Id = 6, Name = "Rateb A", TotalSales = 30000m, Gender = "M"} ,
+                new Employee { Id = 7, Name = "Abeer C", TotalSales = 16000m, Gender = "F"} ,
+                new Employee { Id = 8, Name = "Marwan M", TotalSales = 15000m, Gender = "M"} ,
+
+            };
+            // Before using Delegate
+            Console.WriteLine("\n================ Before Using Delegate ================\n");
+
+            Report report = new Report();
+            report.ProcessEmployee60000PlusSales(emps);
+            report.ProcessEmployeesWithSalesBetween30000And59999(emps);
+            report.ProcessEmployeesWithSalesLess30000(emps);
+
+            Console.ReadKey();
+        }
+
+    }
+
+
+}
---
+++ MulticastDelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace MulticastDelegate
+{
+    public delegate void RecDelegate(decimal width, decimal height);
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var helper = new RectangleHelper();
+            helper.GetArea(10, 10);
+            helper.GetPerimeter(10, 10);
+
+            // After Delegate
+            Console.WriteLine("After Delegate");
+            RecDelegate rect;
+            rect = helper.GetPerimeter;
+            rect += helper.GetArea;
+            rect(10, 10);
+
+            rect -= helper.GetArea;  // unsubscribing 
+
+            Console.WriteLine("After unsbuscribing rect.Area");
+
+            rect(10, 10);
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public class RectangleHelper
+    {
+        public void GetArea(decimal width, decimal height)
+        {
+            var result = width * height;
+            Console.WriteLine($" Area = {width} x {height} = {result}");
+        }
+
+        public void GetPerimeter(decimal width, decimal height)
+        {
+            var result = 2 * (width + height);
+            Console.WriteLine($" Perimeter = 2 x ({width} + {height}) = {result}");
+        }
+    }
+}  
